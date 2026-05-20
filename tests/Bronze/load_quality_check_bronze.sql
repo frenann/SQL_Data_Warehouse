56b7f1945d829f1a54e4cc01638b1ff28f59e154
Renan Costa Fernandes <137432587+frenann@users.noreply.github.com>
2026-05-20 16:00:13 -0300

    Create load_quality_check_bronze.sql for data validation
    Added SQL queries to verify data loading for multiple tables.

---
+++ tests/Bronze/load_quality_check_bronze.sql
@@ -0,0 +1,65 @@
+-- Para checar se as informações foram carregadas de maneira correta:
+
+SELECT *
+FROM bronze.crm_cust_info
+
+-- Para checar se todas as linhas foram carregadas na tabela:
+
+SELECT COUNT(*) AS 'Total de Linhas'
+FROM bronze.crm_cust_info
+
+
+-- Para checar se as informações foram carregadas de maneira correta:
+
+SELECT *
+FROM bronze.crm_prd_info
+
+-- Para checar se todas as linhas foram carregadas na tabela:
+
+SELECT COUNT(*) AS 'Total de Linhas'
+FROM bronze.crm_prd_info
+
+
+-- Para checar se as informações foram carregadas de maneira correta:
+
+SELECT *
+FROM bronze.crm_sales_details
+
+-- Para checar se todas as linhas foram carregadas na tabela:
+
+SELECT COUNT(*) AS 'Total de Linhas'
+FROM bronze.crm_sales_details
+
+
+-- Para checar se as informações foram carregadas de maneira correta:
+
+SELECT *
+FROM bronze.erp_cust_az12
+
+-- Para checar se todas as linhas foram carregadas na tabela:
+
+SELECT COUNT(*) AS 'Total de Linhas'
+FROM bronze.erp_cust_az12
+
+
+-- Para checar se as informações foram carregadas de maneira correta:
+
+SELECT *
+FROM bronze.erp_loc_a101
+
+-- Para checar se todas as linhas foram carregadas na tabela:
+
+SELECT COUNT(*) AS 'Total de Linhas'
+FROM bronze.erp_loc_a101
+
+
+-- Para checar se as informações foram carregadas de maneira correta:
+
+SELECT *
+FROM bronze.erp_px_cat_g1v2
+
+-- Para checar se todas as linhas foram carregadas na tabela:
+
+SELECT COUNT(*) AS 'Total de Linhas'
+FROM bronze.erp_px_cat_g1v2
+
