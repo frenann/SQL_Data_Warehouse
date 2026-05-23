7438ed00b39bbeb61ec2a53607700be63d6ba1e9
Renan Costa Fernandes <137432587+frenann@users.noreply.github.com>
2026-05-23 12:53:06 -0300

    Add quality checks for customer data in SQL
    Added SQL queries to check for duplicate customer IDs, unwanted spaces in first and last names, and consistency of gender values.

--- tests/Silver/load_quality_check_silver.sql
+++ tests/Silver/load_quality_check_silver.sql
@@ -1,1 +1,31 @@
 
+
+-- Para detectar valores de ID dos Clientes (Primary Key) duplicados:
+
+SELECT 
+cst_id AS 'ID do Cliente',
+COUNT (*) AS 'Total'
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT (*) > 1 OR cst_id IS NULL
+
+-- Para checar se existem espaços indesejados:
+
+-- Caso o fisrtname seja diferente do firstname após a aplicação da função TRIM, isso indica que existem caracteres indesejados.
+
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+-- Caso o lastname seja diferente do lastname após a aplicação da função TRIM, isso indica que existem caracteres indesejados.
+
+SELECT cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+-- Para checar a consistência dos valores de cst_gndr:
+
+SELECT DISTINCT (cst_gndr)
+FROM bronze.crm_cust_info
+
+
